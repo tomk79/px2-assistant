d00561b2651f095f37f071df48fbde57ebbb7ff7
Tomoya Koyanagi <tomk79@gmail.com>
2025-04-04 21:49:32 +0900

    時刻取得関数 `getConfig` を追加 (2)

--- cce/src/includes/components/Chat/includes/functions/getConfig.js
+++ cce/src/includes/components/Chat/includes/functions/getConfig.js
@@ -9,9 +9,20 @@ class getConfig {
 
 	async run (args) {
 		return new Promise((resolve, reject) => {
-			const now = new Date().toISOString();
-			console.log('--- getConfig:', now);
-			resolve(now);
+			this.#cceAgent.pxCmd('/?PX=api.get.config',
+				{
+					"progress": function(data, error){
+						console.log('--- progress:', data, error);
+					},
+				},
+				function(pxCmdStdOut, error){
+					console.log('--- getConfig:', pxCmdStdOut, error);
+					if(error){
+						reject(error);
+						return;
+					}
+					resolve(JSON.stringify(pxCmdStdOut));
+				});
 		});
 
 	}
